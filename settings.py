16a135a8e18d6b3fdff68eb815d4527089b332be
Andriy Gula <gula@pr.km.ua>
2023-01-17 21:21:13 +0200

    fix typo

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self) -> None:
         """Initialize the game's settings"""
         self.screen_width = 1200
-        self.screen_haight = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
