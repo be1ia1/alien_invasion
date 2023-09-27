188992a822f9f069f70b9295a0a58a9397a22d40
gulaa <gula@pr.km.ua>
2023-09-27 17:00:26 +0300

    ex12-4

---
+++ ex12-4.py
@@ -0,0 +1,69 @@
+import pygame
+import sys
+
+from rocket import Rocket
+
+
+class BlueSky():
+    
+    def __init__(self) -> None:
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200, 800))
+        self.bg_color = (135, 206, 250)
+        pygame.display.set_caption('Blue Sky')
+        self.rocket = Rocket(self)
+        
+    
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen
+        self.screen.fill(self.bg_color)
+        self.rocket.blitme()
+        pygame.display.flip()
+
+    def _check_events(self):
+        # Respond to keypresses and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                # print(event.key)
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypress"""
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.rocket.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    bs = BlueSky()
+    bs.run_game()
---
+++ rocket.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Rocket():
+    def __init__(self, ex_game) -> None:
+        """Initialize the rocket and set its starting position"""
+        self.screen = ex_game.screen
+        self.screen_rect = ex_game.screen.get_rect()
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rocket_speed = 1.5
+
+        self.rect = self.image.get_rect()
+
+        self.rect.center = self.screen_rect.center
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        # move right
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.rocket_speed
+        # move left
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.rocket_speed
+        # move up
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.rocket_speed
+        # move down
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.rocket_speed
+            
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the rocket at its current location"""
+        self.screen.blit(self.image, self.rect)
